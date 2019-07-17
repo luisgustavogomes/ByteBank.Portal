25d0ab97215336c6fc903f469bbebfa7d2854421
luis gomes <luisgustavogomes@outlook.com>
2019-07-17 14:37:38 -0300

    Criando um listerner do Http

---
+++ ByteBank.Portal/Infraestrutura/WebApplication.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Infraestrutura
+{
+    public class WebApplication
+    {
+        private readonly string[] _prefixos;
+        public WebApplication(string[] prefixos)
+        {
+            _prefixos = prefixos ?? throw new ArgumentNullException(nameof(prefixos));
+        }
+
+        public void Iniciar()
+        {
+            var httpListener = new HttpListener();
+            foreach (var item in _prefixos)
+                httpListener.Prefixes.Add(item);
+
+            httpListener.Start();
+
+            var contexto = httpListener.GetContext();
+            var request = contexto.Request;
+            var response = contexto.Response;
+
+            var respostaConteudo = "Hello World";
+            var respostaConteudoBytes = Encoding.UTF8.GetBytes(respostaConteudo);
+
+            response.ContentType = "text/html; charset=utf=8";
+            response.StatusCode = 200;
+            response.ContentLength64 = respostaConteudoBytes.Length;
+            response.OutputStream.Write(respostaConteudoBytes, 0, respostaConteudoBytes.Length);
+            response.OutputStream.Close();
+
+            httpListener.Stop();
+        }
+    }
+}
